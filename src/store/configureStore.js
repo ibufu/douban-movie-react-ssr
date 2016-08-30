fedd6bebe58ff81d3e9c3b4fc0efd05532b62e60
ibufu <wzl_911@qq.com>
2016-08-30 18:22:54 +0800

    fix: remove useless module

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,6 +1,5 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import createLogger from 'redux-logger';
 import rootReducer from '../reducers';
 import api from '../middleware/api';
 
