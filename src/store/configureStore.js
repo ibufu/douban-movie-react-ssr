4b5dc244d12e88db627445268e5d4f36a65bcb6f
ibufu <wzl_911@qq.com>
2016-08-31 09:55:34 +0800

    refactor: configureStore

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,5 +1,24 @@
-if (__NODE__) {
-  module.exports = require('./configureStore.node')
-} else {
-  module.exports = require('./configureStore.dev')
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from '../reducers';
+import createLogger from 'redux-logger'
+import api from '../middleware/api';
+
+
+const middleware = [
+    thunk,
+    api,
+    !__NODE__ && createLogger(),
+].filter(Boolean);
+
+export default function configureStore(initialState) {
+    const store = createStore(
+        rootReducer,
+        initialState,
+        compose(
+            applyMiddleware(...middleware),
+        )
+    );
+
+    return store;
 }
